06188a4613255395c2ce6d3d1e7549aedbd61f2a
redbird <cs20131575@u.sogang.ac.kr>
2018-12-12 17:39:41 +0900

    done doing channel Statistics

---
+++ channelStatistics.py
@@ -0,0 +1,51 @@
+from config import apikey, host, user, password, db, charset
+import requests
+import json
+import pymysql
+import datetime
+
+
+def getStatistics(channelId):
+    #GET https://www.googleapis.com/youtube/v3/channels
+    url = "https://www.googleapis.com/youtube/v3/channels"
+    params = {'part': 'statistics,snippet', 'id': channelId, 'key': apikey}
+    result = requests.get(url=url, params=params)
+    return result
+
+
+def main():
+    con = pymysql.connect(host=host, user=user, password=password, db=db, charset=charset, autocommit=True)
+    curs = con.cursor()
+    
+
+    sql = "SELECT channelId FROM youtubeChannelList"
+    curs.execute(sql)
+
+    channelIds = curs.fetchall()
+
+    for channelId in channelIds:
+        channelId = channelId[0]
+
+        result = getStatistics(channelId)
+        result = json.loads(result.text)
+        
+        items = result['items'][0]
+        statistics = items['statistics']
+        viewCount = statistics['viewCount']
+        commentCount = statistics['commentCount']
+        subscriberCount = statistics['subscriberCount']
+        videoCount = statistics['videoCount']
+
+        now = datetime.datetime.now()
+        updateTime = now.strftime('%Y-%m-%d %H:%M:%S')
+        
+        sql = "INSERT INTO youtubeChannelStatistics (channelId, updateTime, viewCount, commentCount, subscriberCount, videoCount) VALUES (%s, %s, %s, %s, %s, %s)"
+        curs.execute(sql, (channelId, updateTime, viewCount, commentCount, subscriberCount, videoCount))
+    
+    
+
+    con.close()
+
+if __name__ == "__main__":
+    main()
+    
