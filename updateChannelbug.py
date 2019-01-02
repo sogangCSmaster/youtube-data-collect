7924a04e3edeac90a3412d83afd3a8312fef3b56
redbird <cs20131575@u.sogang.ac.kr>
2019-01-02 11:46:30 +0900

    update channels

--- updateChannelbug.py
+++ updateChannelbug.py
@@ -3,8 +3,6 @@ import requests
 import json
 import pymysql
 import datetime
-from categoryList import categoryList
-from pprint import pprint
 
 
 def mostPopular(maxResults):
@@ -24,7 +22,7 @@ def mostPopular_next(maxResults, nextPageToken):
 def update_channel(channelId):
     #GET https://www.googleapis.com/youtube/v3/channels
     url = "https://www.googleapis.com/youtube/v3/channels"
-    params = {'part': 'statistics,snippet,id,brandingSettings,topicDetails', 'id': channelId, 'key': apikey}
+    params = {'part': 'statistics,snippet', 'id': channelId, 'key': apikey}
     result = requests.get(url=url, params=params)
     return result
 
@@ -32,30 +30,20 @@ def update_channel(channelId):
 def main():
     con = pymysql.connect(host=host, user=user, password=password, db=db, charset=charset, autocommit=True)
     curs = con.cursor()
-    
-    channelIds = []
-    
-    datas = mostPopular(50)
-    datas = json.loads(datas.text) #result to json
-    nextPageToken = datas['nextPageToken']
-    items2 = datas['items']
 
-    for item in items2:
-        snippet = item['snippet']
-        channelId = snippet['channelId']
-        channelIds.append(channelId)
+    sql = "SELECT channelId FROM youtubeChannelList WHERE thumbnails=''"
+    curs.execute(sql)
+    channelIds = curs.fetchall()
 
     f = "%Y-%m-%dT%H:%M:%S.%fZ"
 
     for channelId in channelIds:
-        result = update_channel(channelId)
+        result = update_channel(channelId[0])
         result = json.loads(result.text)
 
         items = result['items'][0]
         snippet = items['snippet']
-        topicDetails = items['topicDetails']
-        topicIds = topicDetails['topicIds']
-        
+
         title = snippet['title']
         description = snippet['description']
         publishedAt = snippet['publishedAt']
@@ -63,21 +51,14 @@ def main():
         thumbnails = snippet['thumbnails']['default']['url']
         sql = "INSERT INTO youtubeChannelList (channelId, title, description, thumbnails, publishedAt) VALUES (%s, %s, %s, %s, %s)"
 
-        for topicId in topicIds:
-            sql2 = "SELECT * FROM channelCategorized WHERE channelId=%s AND topicId=%s"
-            curs.execute(sql2, (channelId, topicId))
-            results = curs.fetchall()
-
-            if not results:
-                sql3 = "INSERT INTO channelCategorized (channelId, channelTitle, topicId, topicName) VALUES (%s, %s, %s, %s)"
-                topicName = categoryList[topicId]
-                curs.execute(sql3, (channelId, title, topicId, topicName))
         try:
             curs.execute(sql, (channelId, title, description, thumbnails, publishedAt))
         except Exception as e:
-            print(e)
-
+            sql2 = "UPDATE youtubeChannelList SET thumbnails=%s WHERE channelId=%s"
+            curs.execute(sql2, (thumbnails, channelId[0]))
     con.close()
+
 
 if __name__ == "__main__":
     main()
+
