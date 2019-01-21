dbf8c4027edfb3d31a38bba6e03c7ce6fa4502fd
redbird <cs20131575@u.sogang.ac.kr>
2019-01-21 12:58:29 +0900

    will add updateKeyword.py

--- test.py
+++ test.py
@@ -35,44 +35,12 @@ def handler():
     
     datas = mostPopular(50)
     datas = json.loads(datas.text) #result to json
-    nextPageToken = datas['nextPageToken']
-    items2 = datas['items']
-
-    for item in items2:
-        snippet = item['snippet']
-        channelId = snippet['channelId']
-        channelIds.append(channelId)
-
-    f = "%Y-%m-%dT%H:%M:%S.%fZ"
-
-    for channelId in channelIds:
-        result = update_channel(channelId)
-        result = json.loads(result.text)
-
-        items = result['items'][0]
-        snippet = items['snippet']
-        topicDetails = items['topicDetails']
-        topicIds = topicDetails['topicIds']
-        
-        title = snippet['title']
-        description = snippet['description']
-        publishedAt = snippet['publishedAt']
-        publishedAt = datetime.datetime.strptime(publishedAt, f)
-        thumbnails = snippet['thumbnails']['default']['url']
-        sql = "INSERT INTO youtubeChannelList (channelId, title, description, thumbnails, publishedAt) VALUES (%s, %s, %s, %s, %s)"
-
-        for topicId in topicIds:
-            sql2 = "SELECT * FROM channelCategorized WHERE channelId=%s AND topicId=%s"
-            curs.execute(sql2, (channelId, topicId))
-            results = curs.fetchall()
-
-            if not results:
-                sql3 = "INSERT INTO channelCategorized (channelId, channelTitle, topicId, topicName) VALUES (%s, %s, %s, %s)"
-                topicName = categoryList[topicId]
-                curs.execute(sql3, (channelId, title, topicId, topicName))
-        try:
-            curs.execute(sql, (channelId, title, description, thumbnails, publishedAt))
-        except Exception as e:
-            print(e)
+    print(datas)
+    
+    sql = "SELECT * FROM youtubeChannelList"
+    curs.execute(sql)
+    rows = curs.fetchall()
+    print(rows)
 
     con.close()
+    return "done"
